bf4d807490ba879ea494a6f61ef951944e7cb5e8
SahilGrip <sahillather002@gmail.com>
2023-12-24 09:22:05 +0530

    navigation added

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import Styles from './navigation.module.css'
+import Link from 'next/link'
 const navigation = () => {
   return (
-    <div>navigation</div>
+    <header className={`${Styles.navigationHeader}`}>
+      <div className={`${Styles.logo}`}>
+        <p>ArtsNft</p>
+      </div>
+      <nav>
+        <ul className={`${Styles.linksHead}`}>
+          <li className={`${Styles.links}`}>
+            <Link href="#">Home</Link>
+          </li>
+          <li className={`${Styles.links}`}>
+            <Link href="#">About</Link>
+          </li>
+          <li className={`${Styles.links}`}>
+            <Link href="#">Services</Link>
+          </li>
+          <li className={`${Styles.links}`}>
+            <Link href="#">Portfolio</Link>
+          </li>
+          <li className={`${Styles.links}`}>
+            <Link href="#">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <button className={`${Styles.navButton}`}>
+        <Link href="#">Connect Wallet</Link>
+      </button>
+    </header>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Slider from "@/components/slider/Slider"
+import Navigation from '@/components/navigation/navigation'
 export default function Home() {
   return (
     <>
+    <Navigation/>
     <Slider/>
     </>
   )
